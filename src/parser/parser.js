a976bd9e6b7f946155a765e8691956b4ee6a4200
bs@cvt127.cvt.dk <bs@cvt127.cvt.dk>
2009-03-20 15:18:00 +0100

    Added more error checking and changed output from list to syntax tree.

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -89,7 +89,7 @@ var parser = function () {
     ];
 
     operators.forEach(function (op) {
-        op.type = 'OPERATOR';
+        op.type = 'BINARY_OPERATOR';
         op.precedence = op.precedence || 10;
         op.associative = op.associative !== undefined ? op.associative : true;
         op.pattern = new RegExp('^' + op.token.replace(/[.*+?^${}()|[\]\/\\]/g, '\\$&'), 'g');
@@ -107,7 +107,7 @@ var parser = function () {
     });
         
     tokens.append(operators, functions, constants, [ { type: 'VARIABLE', pattern: /^[a-zA-Z\.]+/g } ]);
-    project.log(tokens.peek().pattern);
+
     var lex = lexer(tokens);
 
     return {
@@ -115,23 +115,19 @@ var parser = function () {
             var iterator = lex.tokenize(str),
                 token = {},
                 output = [],
-                stack = [];
+                stack = [],
+                v1, v2;
             
             while(token = iterator.next()) {
-                if (token.type === 'NUMBER') {
-                    output.push(token);
-                }
-                else if (token.type === 'CONSTANT') {
-                    output.push(token);
-                }
-                else if (token.type === 'VARIABLE') {
+            
+                if (token.type === 'NUMBER' || token.type === 'CONSTANT' || token.type === 'VARIABLE') {
                     output.push(token);
                 }
                 else if (token.type === 'FUNCTION') {
                     stack.push(token);
                 }
                 else if (token.type === 'COMMA') {
-                    while (stack.peek().type !== 'PARENTHESES_LEFT') {
+                    while (!stack.isEmpty() && stack.peek().type !== 'PARENTHESES_LEFT') {
                         output.push(stack.pop());
                     
                         if (!stack.isEmpty()) {
@@ -139,12 +135,15 @@ var parser = function () {
                         }
                     }
                 }
-                else if (token.type === 'OPERATOR') {                
-                    while (!stack.isEmpty() && stack.peek().type === 'OPERATOR' && (
+                else if (token.type === 'BINARY_OPERATOR') {  
+                    while (!stack.isEmpty() && stack.peek().type === 'BINARY_OPERATOR' && (
                             (token.associative && token.precedence <= stack.peek().precedence) || 
                             (!token.associative && token.precedence < stack.peek().precedence)
                             )) {
-                        output.push(stack.pop());
+                        v1 = output.pop();
+                        v2 = output.pop();
+                        
+                        output.push([stack.pop(), v1, v2]);
                     }
                     stack.push(token);
                 }
@@ -152,28 +151,44 @@ var parser = function () {
                     stack.push(token);
                 }
                 else if (token.type === 'PARENTHESES_RIGHT') {
-                    while (stack.peek().type !== 'PARENTHESES_LEFT') {
-                        output.push(stack.pop());
+                    while (!stack.isEmpty() && stack.peek().type !== 'PARENTHESES_LEFT') {
+                        v1 = output.pop();
+                        v2 = output.pop();
+                    
+                        output.push([stack.pop(), v1, v2]);
                         
                         if (stack.isEmpty()) {
                             throw new Error('Mismatched parentheses.');
                         }
                     }
                     stack.pop();
-                    if (stack.peek().type === 'FUNCTION') {
+                    if (!stack.isEmpty() && stack.peek().type === 'FUNCTION') {
                         output.push(stack.pop());
                     }
                 }
             }
+ 
             
             if (!stack.isEmpty()) {
-                project.log(stack);
                 while (!stack.isEmpty()) {
                     if (stack.peek().type === 'PARENTHESES_LEFT' || stack.peek().type === 'PARENTHESES_RIGHT') {
                         throw new Error('Mismatched parentheses.');
                     }
-                    output.push(stack.pop());
+                    v1 = output.pop();
+                    v2 = output.pop();
+                    
+                    if (v1 === undefined || v2 === undefined) {
+                        throw new Error('Binary operator "' + stack.peek().token + '" requires two arguments.');
+                    }
+                    
+                    output.push([stack.pop(), v1, v2]);
                 }
+            }
+            
+            if (output.length !== 1) {
+                project.log("input created more than one parse tree");
+                project.log(output[0]);
+                project.log(output[1]);
             }
             return output;
         }
--- test/parser/parser.test.js
+++ test/parser/parser.test.js
@@ -5,23 +5,51 @@ eval(loadFile("src/parser/parser.js"));
 
 //var p;
 
+function evaluate(tree) {
+ project.log(Object.isArray(tree));
+
+
+    if (tree.type === 'NUMBER') {
+        return tree.value;
+    }
+    else if (tree[0].type === 'BINARY_OPERATOR') {
+        if (tree[0].token === '+') {
+            return evaluate(tree[1]) + evaluate(tree[2]);
+        }
+        else if (tree[0].token === '*') {
+            return evaluate(tree[1]) * evaluate(tree[2]);
+        }
+    }
+     }
+
+
 testCases(test, 
 	function setUp() {
      //   p = parser();
 	},
     
     function checkIncorrectEnd() {
-        project.log(parser.parse('4 *'));
+        function f() {
+            parser.parse('4 * (5 + ) / ');
+        }
+    
+        shouldThrowException(function () {
+            f();
+        });
     },
- /*   
-    function checkParser() {
-        project.log(parser.parse('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 2 *'));
+    
+    function checkSimple() {
+        assert.that(evaluate(parser.parse('3 + 4')), eq(7));
+    },
+    
+    function checkNesting() {
+        //project.log(parser.parse('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 2'));
     },
         
     function checkFunction() {
-        project.log(parser.parse('sin(1, cos (4), 6)'));
+    //    project.log(parser.parse('sin(1, cos (4), 6)'));
     },
-*/
+
     function tearDown() {
   //      p = null;
     }
