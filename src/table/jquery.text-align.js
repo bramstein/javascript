1c87b17bfaab76e80980e398f7d4e4f196a18ab4
bs@bram <bs@bram>
2009-06-23 08:25:42 +0200

    First version of text align plugin.

---
+++ src/table/jquery.text-align.js
@@ -0,0 +1,46 @@
+/*!
+ * jQuery Text Alignment v0.02
+ *
+ * Licensed under the new BSD License.
+ * Copyright 2009, Bram Stein
+ * All rights reserved.
+ */
+/*global jQuery*/
+(function ($) {
+	$.extend(jQuery.fn, {
+		textAlign: function (string) {
+			var items = $(this),
+				maxLeft = 0,
+				maxRight = 0;
+
+			if (string === 'start' || string === 'end' || string === 'left' || string === 'right' || string === 'justify') {
+				return items.css({textAlign: string});
+			} else if (string.length === 1) {
+				items.each(function () {
+					var that = $(this),
+						str = that.text(),
+						left = $('<span>' + str.slice(0, str.indexOf(string)) + '</span>'),
+						right = $('<span>' + str.slice(str.indexOf(string)) + '</span>');
+
+					left.css({display: 'inline-block', textAlign: 'right'});
+					right.css({display: 'inline-block', textAlign: 'left'});
+
+					$(this).empty().append(left).append(right);
+
+					maxLeft = Math.max(maxLeft, left.width());
+					maxRight = Math.max(maxRight, right.width());
+				});
+
+				items.each(function () {
+					var children = $(this).children(),
+						left = $(children[0]),
+						right = $(children[1]);
+				
+					left.width(maxLeft);
+					right.width(maxRight);
+				});
+			}
+			return items;
+		}
+	});
+})(jQuery);
