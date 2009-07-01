847a8a6e86978433fb8f71af7cc19c2b7aa1aa55
bs@cvt121.cvt.dk <bs@cvt121.cvt.dk>
2009-07-01 15:22:17 +0200

    Enabled default Canvas text API instead of custom mozilla one (now that 3.5 has been released.)

--- src/chart/text.canvas.js
+++ src/chart/text.canvas.js
@@ -2,24 +2,41 @@
 /*global defaults*/
 Object.extend(defaults.text, {
 	fill: function (context, x, y, str, options) {
-		if (options.textAlign) {
+        var offset = {x: 0, y: 0},
+            numerical = (typeof str === 'number' && !isNaN(str)),
+            padding = {
+                top: 0,
+                bottom: 0,
+                right: 0,
+                left: 0
+            };
+    
+        Object.forEach(padding, function (v, k) {
+			padding[k] = options.padding && options.padding[k] || v;
+		});
+    
+		if (options.textAlign) {        
 			context.textAlign = options.textAlign;
 		}
-	
+        
+        if (options.textBaseLine) {
+            context.textBaseline = options.textBaseLine;
+        }
+    
 		if (options.font) {
 			context.font = options.font.toString();
 		}
 
 		context.save();
 		context.scale(1, -1);
-		context.fillText(str, x, -y);
+		context.fillText(str, x + padding.left - padding.right, -y + padding.top - padding.bottom);
 		context.restore();
 	},
 	stroke: function (context, x, y, str, options) {
 		if (options.textAlign) {
 			context.textAlign = options.textAlign;
 		}
-	
+    
 		if (options.font) {
 			context.font = options.font.toString();
 		}
@@ -40,7 +57,7 @@ Object.extend(defaults.text, {
 		
 		if (str !== undefined) {
 			result.width = context.measureText(str).width;
-			result.height = font.size * 0.85;
+			result.height = font.size * 0.80;
 		}
 		context.font = previousFont;
 	}
